297ec166fe434c2da7cb750b7e7eb92412b47cc0
Guillaume Robin <guirobin37@gmail.com>
2023-04-28 14:36:13 +0200

    SMTP send done (no testing yet)

--- projet.sh
+++ projet.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-##-------------------------------------------------VARIABLE SETTING-----------------------------
-#SMTP_SERVER="your_smtp_server"
-#SMTP_LOGIN="your_smtp_login"
-#SMTP_PASSWORD="your_smtp_password"
-
 #-------------------------------------------------ACCOUNT CREATION----------------------------
 #sed -i 1d accounts.csv # remove first line for Name Surname password etc ..
 mkdir /home/shared
@@ -16,27 +10,15 @@ do
   last_name=$(echo "$line" | cut -d';' -f2) # take surname column 2
   username="$(echo $first_name | head -c 1)${last_name}" # concatenation
   password=$(echo "$line" | cut -d';' -f4) # take password column 4
-  sudo useradd -p $(openssl passwd -1 $password) $username -m -d /home/share/$username
+  mailAdress=$(echo "$line" | cut -d';' -f3) # take mail  column 3
+  sudo useradd -p $(openssl passwd -1 $password) $username -m -d /home/shared/$username
   #sudo usermod -p $password $username
   #sudo passwd -e $username # make password expire immediatly
   mkdir /home/shared/$username/a_sauver
   echo "$username"
     # send email to user
-    #EMAIL_SUBJECT="Welcome to Linux Project $HOSTNAME"
-    #EMAIL_BODY="Your acccount as succesfully been created, thank you ! For security reason, please change your password" 
+    mailx -S smtp=10.30.48.100 -r guirobin37@gmail.com -s "Account creation" -v "$mailAdress" < "Hello, your username is : "$username" and your password is : "$password". Please change your password for security reason. Thank you !"
 done < accounts.csv
-
-
-
-
-
-
-
-
-
-
-
-
 
 # ---------------------------------------------------ECPLISE INSTALLATION -------------------------------------------------
 # apt install eclipse # ? not with apt ?  
