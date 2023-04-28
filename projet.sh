737643d530a06ccbd1b13295030148846c30c94d
Guillaume Robin <guirobin37@gmail.com>
2023-04-28 14:15:19 +0200

    fix special characters password error

--- projet.sh
+++ projet.sh
@@ -2,9 +2,9 @@
 
 
 ##-------------------------------------------------VARIABLE SETTING-----------------------------
-SMTP_SERVER="your_smtp_server"
-SMTP_LOGIN="your_smtp_login"
-SMTP_PASSWORD="your_smtp_password"
+#SMTP_SERVER="your_smtp_server"
+#SMTP_LOGIN="your_smtp_login"
+#SMTP_PASSWORD="your_smtp_password"
 
 #-------------------------------------------------ACCOUNT CREATION----------------------------
 #sed -i 1d accounts.csv # remove first line for Name Surname password etc ..
@@ -14,17 +14,16 @@ while read line
 do
   first_name=$(echo "$line" | cut -d';' -f1) # take name column 1
   last_name=$(echo "$line" | cut -d';' -f2) # take surname column 2
-  #username="$(echo $first_name | head -c 1)${last_name}" # concatenation
-  username=$(echo $first_name $last_name | tr '[:upper:]' '[:lower:]' | tr -d ' ' | cut -c1-8) # concatenation
+  username="$(echo $first_name | head -c 1)${last_name}" # concatenation
   password=$(echo "$line" | cut -d';' -f4) # take password column 4
-  echo "$username:$password" | chpasswd 
-  sudo useradd $username -m -d /home/shared/$username
-  sudo passwd --expire $username # make password expire immediatly
+  sudo useradd -p $(openssl passwd -1 $password) $username -m -d /home/share/$username
+  #sudo usermod -p $password $username
+  #sudo passwd -e $username # make password expire immediatly
   mkdir /home/shared/$username/a_sauver
   echo "$username"
     # send email to user
-    EMAIL_SUBJECT="Welcome to Linux Project $HOSTNAME"
-    EMAIL_BODY="Your acccount as succesfully been created, thank you ! For security reason, please change your password" 
+    #EMAIL_SUBJECT="Welcome to Linux Project $HOSTNAME"
+    #EMAIL_BODY="Your acccount as succesfully been created, thank you ! For security reason, please change your password" 
 done < accounts.csv
 
 
--- remove.sh
+++ remove.sh
@@ -3,8 +3,10 @@
 # remove all users created from accounts.csv file
 while read line
 do 
-  first_name=$(echo "$name" | cut -d';' -f1)
-  last_name=$(echo "$surname" | cut -d';' -f2)
+  first_name=$(echo "$line" | cut -d';' -f1)
+  last_name=$(echo "$line" | cut -d';' -f2)
   username="$(echo $first_name | head -c 1)${last_name}"
-  userdel -rf $username # remove user and their home directory
+  userdel -r $username # remove user and their home directory
+  rm -rf /home/shared/$username # remove the directory created for the user
 done < accounts.csv
+
